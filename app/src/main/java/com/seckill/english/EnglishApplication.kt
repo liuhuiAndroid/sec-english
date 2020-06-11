d536438dd62fa569920231e3db08257725d8ca21
lh <im.lh@hotmail.com>
2020-06-11 18:56:57 +0800

    集成环信

--- app/src/main/java/com/seckill/english/EnglishApplication.kt
+++ app/src/main/java/com/seckill/english/EnglishApplication.kt
@@ -6,7 +6,34 @@ import android.widget.Toast
 import com.google.android.gms.common.GoogleApiAvailability
 import com.google.android.gms.tasks.OnCompleteListener
 import com.google.firebase.iid.FirebaseInstanceId
+import com.hyphenate.chat.EMClient
+import com.hyphenate.chat.EMOptions
 
 class EnglishApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        initEM()
+    }
+
+    /**
+     * 初始化环信SDK
+     */
+    private fun initEM() {
+        val options = EMOptions().apply {
+            // 默认添加好友时，是不需要验证的，改成需要验证
+            acceptInvitationAlways = false
+            // 设置自动登录
+            autoLogin = true
+            // 是否自动将消息附件上传到环信服务器，默认为True是使用环信服务器上传下载，如果设为 false，需要开发者自己处理附件消息的上传和下载
+            autoTransferMessageAttachments = true
+            // 是否自动下载附件类消息的缩略图等，默认为 true 这里和上边这个参数相关联
+            setAutoDownloadThumbnail(true)
+        }
+        // 初始化
+        EMClient.getInstance().init(applicationContext, options)
+        // 在做打包混淆时，关闭debug模式，避免消耗不必要的资源
+        EMClient.getInstance().setDebugMode(BuildConfig.DEBUG)
+    }
 
 }
--- app/src/main/java/com/seckill/english/ui/SplashActivity.kt
+++ app/src/main/java/com/seckill/english/ui/SplashActivity.kt
@@ -1,12 +1,18 @@
 package com.seckill.english.ui
 
+import android.R.attr.password
 import android.os.Bundle
 import android.util.Log
-import com.google.android.gms.common.GoogleApiAvailability
 import com.google.android.gms.tasks.OnCompleteListener
 import com.google.firebase.iid.FirebaseInstanceId
+import com.hyphenate.EMCallBack
+import com.hyphenate.EMMessageListener
+import com.hyphenate.chat.EMClient
+import com.hyphenate.chat.EMMessage
 import com.seckill.english.R
 import com.seckill.english.base.BaseActivity
+import kotlinx.android.synthetic.main.activity_splash.*
+
 
 class SplashActivity : BaseActivity() {
 
@@ -20,15 +26,61 @@ class SplashActivity : BaseActivity() {
         FirebaseInstanceId.getInstance().instanceId
             .addOnCompleteListener(OnCompleteListener { task ->
                 if (!task.isSuccessful) {
-                    // java.io.IOException: SERVICE_NOT_AVAILABLE
-                    // Method threw 'com.google.android.gms.tasks.RuntimeExecutionException' exception.
-                    Log.d(TAG, "检索令牌失败：exception = ${task.exception}")
+                    mTvFirebaseToken.text = "检索令牌失败：exception = ${task.exception}"
                     return@OnCompleteListener
                 }
                 // Get new Instance ID token
                 val token = task.result?.token
                 Log.d(TAG, "检索当前注册令牌：$token")
+                mTvFirebaseToken.text = "检索当前注册令牌：$token"
             })
+
+        EMClient.getInstance().login("im-001", "im-001", object : EMCallBack {
+            //回调
+            override fun onSuccess() {
+                Log.d("main", "登录聊天服务器成功！")
+                // 注册环信消息监听
+                EMClient.getInstance().chatManager().addMessageListener(msgListener)
+                runOnUiThread { mTvEmLoginInfo.text = "${EMClient.getInstance().currentUser}登录成功" }
+            }
+
+            override fun onProgress(progress: Int, status: String) {}
+            override fun onError(code: Int, message: String) {
+                Log.d("main", "登录聊天服务器失败！")
+                runOnUiThread { mTvEmLoginInfo.text = "${EMClient.getInstance().currentUser}登录失败" }
+            }
+        })
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        EMClient.getInstance().chatManager().removeMessageListener(msgListener)
+    }
+
+    private val msgListener = object : EMMessageListener {
+        override fun onMessageRecalled(messages: MutableList<EMMessage>?) {
+            Log.d(TAG, "onMessageRecalled")
+        }
+
+        override fun onMessageChanged(message: EMMessage?, change: Any?) {
+            Log.d(TAG, "onMessageChanged")
+        }
+
+        override fun onCmdMessageReceived(messages: MutableList<EMMessage>?) {
+            Log.d(TAG, "onCmdMessageReceived")
+        }
+
+        override fun onMessageReceived(messages: MutableList<EMMessage>?) {
+            Log.d(TAG, "onMessageReceived")
+        }
+
+        override fun onMessageDelivered(messages: MutableList<EMMessage>?) {
+            Log.d(TAG, "onMessageDelivered")
+        }
+
+        override fun onMessageRead(messages: MutableList<EMMessage>?) {
+            Log.d(TAG, "onMessageRead")
+        }
     }
 
 }
